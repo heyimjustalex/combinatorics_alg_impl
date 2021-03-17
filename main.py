ec420edda3f54ae8fbbd2d14dd9d8f4909db3be1
heyimjustalex <21158649+heyimjustalex@users.noreply.github.com>
2021-03-17 15:46:39 +0100

    Add class implementation and example use

--- main.py
+++ main.py
@@ -1,42 +1,109 @@
-output_list = []
-temp_input = []
+def printList(x):
+    for i in x:
+        print(i)
+
 def swapListEl(in_list, i, j):
     temp = in_list[i]
     in_list[i] = in_list[j]
     in_list[j] = temp
 
-def permutacje_bez_powt(input_list,size):
-    if size == 1:
-        output_list.append(list(input_list))
-        #print(output_list)
 
-    else:
-        for i in range (0,size):
-            permutacje_bez_powt(input_list,size-1)
+class Combinatorics:
+    output_l = []
+    output_l2 = []
 
-            if size % 2:
-                swapListEl(input_list,0,size-1)
-            else:
-                swapListEl(input_list,i,size-1)
+    def permutations(self, input_l, t_size):
 
+        if t_size == 1:
+            if input_l not in self.output_l:
+                self.output_l.append(list(input_l))
+        else:
+            for i in range(0, t_size):
+                self.permutations(self, input_l, t_size - 1)
 
-def genList(n):
-    for i in range (1,n+1):
-        temp_input.append(i)
+                if t_size % 2:
+                    swapListEl(input_l, 0, t_size - 1)
+                else:
+                    swapListEl(input_l, i, t_size - 1)
+
+    def combinations(self, k, n, input_l, end=None, start=0, build_l=[]):
+        if not end:
+            end = n
+        if len(build_l) == k:
+            if build_l not in self.output_l2:
+                self.output_l2.append(list(build_l))
+
+        else:
+            for i in range(start, end):
+                build_l.append(input_l[i])
+                self.combinations(self, k, n, input_l, n, i + 1, build_l)
+                build_l.pop()
+
+    def nonRepetitionVariations(self, k, n, input_l):
+        self.permutations(self, input_l, n)
+        for i in self.output_l:
+            self.combinations(self, k, n, i)
+
+    def repetitionVariations(self, k, input_l, build_l=[], start=0):
+        n = len(input_l)
+        if k==0:
+         #  print(build_l)
+           self.output_l.append(list(build_l))
+
+        else:
+            for i in range(start, n):
+                build_l.append(input_l[i])
+                self.repetitionVariations(self, k-1, input_l, build_l, start)
+        if build_l:
+            build_l.pop()
+
+    def cleanOutput(self):
+        self.output_l = []
+        self.output_l2 = []
+
+    def getPermutations(self, input_l, t_size):
+        self.cleanOutput(self)
+        self.permutations(self, input_l, t_size)
+        return self.output_l
+
+    def getCombinations(self, k, n, input_l):
+        self.cleanOutput(self)
+        self.combinations(self, k, n, input_l)
+        return self.output_l2
+
+    def getNonRepVariations(self, k, n, input_l):
+        self.cleanOutput(self)
+        self.nonRepetitionVariations(self, k, n, input_l)
+        return self.output_l2
+
+    def getRepVariations(self, k, input_l):
+        self.cleanOutput(self)
+        self.repetitionVariations(self, k, input_l)
+        return self.output_l
+
 
 if __name__ == '__main__':
-    counter = 0
-    genList(3)
-    permutacje_bez_powt(temp_input, len(temp_input))
-    for i in output_list:
-        print(i)
-        counter = counter + 1
+    inl = ['1', '2', '3']
+    test = Combinatorics
 
-    print("Liczba permutacji:", counter)
+    x = test.getPermutations(test, inl, len(inl))
+    print("Permutations: \n")
+    printList(x)
 
+    print('\n')
 
+    y = test.getCombinations(test, 2, 3, inl)
+    print("Combinations: \n")
+    printList(y)
 
+    print('\n')
 
+    z = test.getNonRepVariations(test, 2, 3, inl)
+    print("Non-Rep Variations: \n")
+    printList(z)
 
+    print('\n')
 
-
+    print("Rep Variations: \n")
+    f = test.getRepVariations(test,4,inl)
+    printList(f)
